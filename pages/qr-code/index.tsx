91686a05883cd2ab1139fd43ab0b69591ce8b3fb
vasilis_litsas <vasilis.litsas@architechts.gr>
2022-01-19 15:34:36 +0200

    added logos in qrcode screen

--- pages/qr-code/index.tsx
+++ pages/qr-code/index.tsx
@@ -1,6 +1,7 @@
 import styles from '../../styles/QR-Code.module.css';
 import QRCode from "react-qr-code";
 import { Stack, Typography } from '@mui/material';
+import Image from 'next/image';
 
 export default function QRcode() {
   return (
@@ -8,6 +9,10 @@ export default function QRcode() {
       <Stack direction={"column"} spacing={2}>
         <Typography variant='h4' align={'center'}>Scan QR code om jouw plekje te bemachtigen</Typography>
         <QRCode value="hey" />
+        <Stack direction={"row"} alignItems={"center"} spacing={1} className={styles.qrCodeImages}>
+          <img src="/assets/images/logo.png" alt="Camping2" width={120} height={50} />
+          <img src="/assets/images/trinit-logo.png" alt="Camping2" width={170} height={170} />
+        </Stack>
       </Stack>
     </div>
   )
